7af0ad919971d869e121c489f61abd7a6b076742
Bjørn Nørgaard <bjornsnorgaard@gmail.com>
2024-07-11 11:25:15 +0200

    Hiding nav elements based on env

--- src/lib/components/NavSide.svelte
+++ src/lib/components/NavSide.svelte
@@ -12,12 +12,14 @@
         </svelte:fragment>
 
         {#each navRoute as r}
-            <AppRailAnchor href={r.route} title={r.label} selected={$page.url.pathname.includes(r.route) && r.route !== "/"}>
-                <svelte:fragment slot="lead">
-                    <svelte:component this={r.icon}/>
-                </svelte:fragment>
-                <span>{r.label}</span>
-            </AppRailAnchor>
+            {#if r.show}
+                <AppRailAnchor href={r.route} title={r.label} selected={$page.url.pathname.includes(r.route) && r.route !== "/"}>
+                    <svelte:fragment slot="lead">
+                        <svelte:component this={r.icon}/>
+                    </svelte:fragment>
+                    <span>{r.label}</span>
+                </AppRailAnchor>
+            {/if}
         {/each}
     </AppRail>
 </div>
--- src/lib/constants/navigation.ts
+++ src/lib/constants/navigation.ts
@@ -1,16 +1,18 @@
 import { BookHeart, Library, Rabbit, Swords } from "lucide-svelte";
 import { routes } from "$lib/constants/routes";
+import { dev } from "$app/environment";
 
 export interface NavRoute {
     label: string;
     route: string;
     icon: any;
+    show: boolean;
 }
 
 export const navRoute: NavRoute[] = [
-    // { label: "Home", route: routes.home(), icon: Home },
-    { label: "Combat", route: routes.combat(), icon: Swords },
-    { label: "Beasts", route: routes.beasts(), icon: Rabbit },
-    { label: "Spells", route: routes.spells(), icon: Library },
-    { label: "Sheet", route: routes.characters(), icon: BookHeart },
+    // { label: "Home", route: routes.home(), icon: Home, show: true },
+    { label: "Combat", route: routes.combat(), icon: Swords, show: true },
+    { label: "Beasts", route: routes.beasts(), icon: Rabbit, show: true },
+    { label: "Spells", route: routes.spells(), icon: Library, show: true },
+    { label: "Sheet", route: routes.characters(), icon: BookHeart, show: dev },
 ];
