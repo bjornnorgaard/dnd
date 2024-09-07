08604b098b4e63e73940b56277d06410f39f5ae3
Bjørn Nørgaard <bjornsnorgaard@gmail.com>
2024-09-07 22:28:56 +0200

    Updated text color for PageSection.svelte and PageWrapper.svelte

--- src/lib/components/PageWrapper.svelte
+++ src/lib/components/PageWrapper.svelte
@@ -8,7 +8,7 @@
 <div class="mx-auto max-w-screen-md px-4 space-y-8 py-[2vw]">
     <AppBar background="bg-transparent" padding="">
         <svelte:fragment slot="headline">
-            <h1 class="bg-gradient-to-br box-decoration-clone bg-clip-text font-bold text-transparent h1 from-primary-500 to-primary-800">{title}</h1>
+            <h1 class="text-primary-500 h1 font-bold">{title}</h1>
             {#if desc}
                 <p>{desc}</p>
             {/if}
