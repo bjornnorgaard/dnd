ac5fab47c2afbc5f4770a542e311189d3870ea54
Bjørn Nørgaard <bjornsnorgaard@gmail.com>
2024-09-13 21:43:43 +0200

    Style update for spellbook

--- src/routes/spellbook/+page.svelte
+++ src/routes/spellbook/+page.svelte
@@ -2,7 +2,7 @@
     import { spellbook } from "$lib/stores/spellbook";
     import PageWrapper from "$lib/components/PageWrapper.svelte";
     import { Accordion, AccordionItem, Tab, TabGroup } from "@skeletonlabs/skeleton";
-    import { AlertCircle, Search } from "lucide-svelte";
+    import { AlertCircle, PlusCircle, Search } from "lucide-svelte";
     import { activeSpellbookIndex } from "$lib/stores/activeSpellbookIndex";
     import { DEFAULT_PAGE_SIZE } from "$lib/constants/paging";
     import type { Spell } from "$lib/types/spell";
@@ -19,15 +19,22 @@
 
     function createSpellbook() {
         if (!ownerInput) {
-            console.log("No input");
             return;
         }
 
-        $spellbook = [ { name: ownerInput, spells: [] }, ...$spellbook ];
+        if ($spellbook.find(b => b.name === ownerInput)) {
+            return;
+        }
+
+        $spellbook = [ ...$spellbook, { name: ownerInput, spells: [] } ];
+        $activeSpellbookIndex = $spellbook.length - 1;
         ownerInput = "";
     }
 
     function deleteSpellbook(index: number) {
+        if ($activeSpellbookIndex === index) {
+            $activeSpellbookIndex = index - 1;
+        }
         $spellbook = $spellbook.filter((_, i) => i !== index);
     }
 
@@ -41,10 +48,19 @@
     }
 
     function addSpell(spell: Spell) {
+        if (!spell?.slug.length) {
+            return;
+        }
+
+        if ($spellbook[$activeSpellbookIndex].spells.find(s => s.slug === spell.slug)) {
+            searchInput = "";
+            spells = [];
+            return;
+        }
         $spellbook[$activeSpellbookIndex].spells = [ ...$spellbook[$activeSpellbookIndex].spells, spell ];
         sortSpellbook($activeSpellbookIndex);
-        spells = [];
         searchInput = "";
+        spells = [];
     }
 
     function sortSpellbook(index: number) {
@@ -57,7 +73,7 @@
             return;
         }
 
-        if (e.key === "Enter") {
+        if (e.key === "Enter" && spells.length) {
             addSpell(spells[0]);
             return;
         }
@@ -68,22 +84,6 @@
 </script>
 
 <PageWrapper title="Spellbook" desc="Manage your spells">
-    <form on:submit|preventDefault={() => createSpellbook()} class="flex gap-4">
-        <label for="input" class="w-full label">
-            <span>Who's spellbook will this be?</span>
-            <div class="flex w-full gap-4">
-                <input id="input" type="text" class="input" autocomplete="off" bind:value={ownerInput} placeholder="What should we call this spellbook?">
-                <button class="btn variant-filled-primary" type="button" disabled={!ownerInput} on:click={() => createSpellbook()}>
-                    {#if ownerInput.length >= 2}
-                        Create {ownerInput}'s spellbook
-                    {:else}
-                        Create
-                    {/if}
-                </button>
-            </div>
-        </label>
-    </form>
-
     {#if $spellbook.length === 0}
         <aside class="flex flex-col alert variant-ghost-warning">
             <div class="flex items-center gap-4">
@@ -94,6 +94,21 @@
                 <p>Use the input above to create your first spellbook.</p>
             </div>
         </aside>
+
+        <form on:submit|preventDefault={() => createSpellbook()} class="flex gap-4">
+            <label for="input" class="w-full label">
+                <span>Who will own this spellbook?</span>
+                <div class="flex w-full gap-2">
+                    <input id="input" type="text" class="input" autocomplete="off" bind:value={ownerInput} placeholder="Character name...">
+                    <button class="btn"
+                            class:variant-filled-surface={!ownerInput.length}
+                            class:variant-filled-primary={ownerInput.length}
+                            type="button" disabled={!ownerInput} on:click={() => createSpellbook()}>
+                        Create
+                    </button>
+                </div>
+            </label>
+        </form>
     {:else}
         <TabGroup active="border-b-2 border-surface-500">
             {#each $spellbook as b, bi}
@@ -106,52 +121,79 @@
                     </h2>
                 </Tab>
             {/each}
+            <Tab bind:group={$activeSpellbookIndex} name="new" value={99}>
+                <div class="btn-icon btn-icon-sm"
+                     class:text-surface-500={$activeSpellbookIndex !== 99}
+                     class:text-primary-500={$activeSpellbookIndex === 99}>
+                    <PlusCircle/>
+                </div>
+            </Tab>
+
             <svelte:fragment slot="panel">
                 <Accordion spacing="space-y-2" regionPanel="variant-soft-surface" regionControl="variant-soft-surface">
-                    <div class="p-4 card variant-soft-surface">
-
-                        <p>Search and add spells to {$spellbook[$activeSpellbookIndex].name}'s spellbook</p>
-                        <div class="input-group input-group-divider grid-cols-[auto_1fr_auto]">
-                            <div class="input-group-shim">
-                                <Search/>
-                            </div>
-                            <input type="search" placeholder="Search..." bind:value={searchInput} on:keydown={(e) => {searchKeydown(e)}}/>
-                        </div>
-                    </div>
-
-                    {#if spells.length}
-                        <Table>
-                            <TableHead>
-                                <th class="">Level</th>
-                                <th>Name</th>
-                                <th class="">School</th>
-                                <th>Duration</th>
-                            </TableHead>
-                            <TableBody>
-                                {#each spells as s}
-                                    <tr role="button" on:click={() => addSpell(s)}>
-                                        <td class="">{s.spell_level}</td>
-                                        <td class="anchor">
-                                            <a href={routes.spells_slug(s.slug)}>
-                                                {s.name}
-                                            </a>
-                                        </td>
-                                        <td class="">{s.school}</td>
-                                        <td>{s.duration}</td>
-                                    </tr>
-                                {/each}
-                            </TableBody>
-                        </Table>
+                    {#if $activeSpellbookIndex === 99 || $spellbook.length === 0}
+                        <form on:submit|preventDefault={() => createSpellbook()} class="flex gap-4">
+                            <label for="input" class="w-full label">
+                                <span>Who will own this spellbook?</span>
+                                <div class="flex w-full gap-2">
+                                    <input id="input" type="text" class="input" autocomplete="off" bind:value={ownerInput} placeholder="Character name...">
+                                    <button class="btn"
+                                            class:variant-filled-surface={!ownerInput.length}
+                                            class:variant-filled-primary={ownerInput.length}
+                                            type="button" disabled={!ownerInput} on:click={() => createSpellbook()}>
+                                        Create
+                                    </button>
+                                </div>
+                            </label>
+                        </form>
                     {/if}
+                    {#if $activeSpellbookIndex !== 99}
+                        <div class="mb-8">
+                            <p class="py-1">Search and select to add spells</p>
+                            <div class="input-group input-group-divider grid-cols-[auto_1fr_auto]">
+                                <div class="input-group-shim">
+                                    <Search/>
+                                </div>
+                                <input class="input" type="search" placeholder="Search spells..."
+                                       bind:value={searchInput}
+                                       on:keydown={(e) => searchKeydown(e)}/>
+                            </div>
+                            <p class="py-1">Use <kbd class="kbd">enter</kbd> to add</p>
+                        </div>
 
-                    {#if !spells.length}
+                        {#if spells.length}
+                            <Table>
+                                <TableHead>
+                                    <th class="">Level</th>
+                                    <th>Name</th>
+                                    <th class="">School</th>
+                                    <th>Duration</th>
+                                </TableHead>
+                                <TableBody>
+                                    {#each spells as s}
+                                        <tr role="button" on:click={() => addSpell(s)}>
+                                            <td class="">{s.spell_level}</td>
+                                            <td class="anchor">
+                                                <a href={routes.spells_slug(s.slug)}>
+                                                    {s.name}
+                                                </a>
+                                            </td>
+                                            <td class="">{s.school}</td>
+                                            <td>{s.duration}</td>
+                                        </tr>
+                                    {/each}
+                                </TableBody>
+                            </Table>
+                        {/if}
+
                         {#each $spellbook[$activeSpellbookIndex].spells as s, si (s.slug)}
                             <div in:slide={{axis: 'y', easing: cubicInOut, delay: 200, duration: 200}} out:slide={{axis: 'y',  easing: cubicInOut, duration: 200}}>
                                 <AccordionItem>
                                     <svelte:fragment slot="summary">
                                         <div class="flex justify-between gap-4">
-                                            <div class="flex gap-4">
-                                                <span class="badge variant-outline">{s.level}</span>
+                                            <div class="flex items-center gap-4">
+                                                <span class="badge badge-icon variant-outline sm:hidden">{s.level_int < 1 ? "C" : s.level_int}</span>
+                                                <span class="hidden badge variant-outline sm:block">{s.level}</span>
                                                 <span class="font-bold text-primary-500">{s.name}</span>
                                             </div>
                                             <div class="flex gap-2">
@@ -159,7 +201,7 @@
                                                     <span class="badge variant-outline-primary">{s.can_be_cast_as_ritual ? "Ritual" : ""}</span>
                                                 {/if}
                                                 {#if s.casting_time !== "1 action"}
-                                                    <span class="badge variant-outline-secondary">{s.casting_time.slice(0, 10)}</span>
+                                                    <span class="hidden badge variant-outline-secondary sm:block">{s.casting_time.slice(0, 10)}</span>
                                                 {/if}
                                                 {#if s.range.toLowerCase() !== "self"}
                                                     <span class="hidden badge variant-outline-tertiary sm:block">{s.range}</span>
@@ -172,34 +214,38 @@
                                         </div>
                                     </svelte:fragment>
                                     <svelte:fragment slot="content">
-                                        <div class="relative">
-                                            <button class="absolute top-0 right-0 btn btn-sm variant-soft-error hover:variant-filled-error" on:click={() => removeSpell($activeSpellbookIndex, si)}>
-                                                Remove {s.name}
-                                            </button>
-                                            <p><b>School</b> {s.level} {s.school.toLowerCase()} {s.can_be_cast_as_ritual ? "(ritual)" : ""}</p>
-                                            <p><b>Casting Time</b> {s.casting_time}</p>
-                                            <p><b>Range</b> {s.range}</p>
-                                            <p><b>Components</b> {s.components}</p>
-                                            {#if s.requires_material_components}
-                                                <p><b>Materials</b> {s.material}</p>
+                                        <div class="px-2 pt-2 pb-4">
+                                            <div class="relative">
+                                                <button class="absolute top-0 right-0 btn btn-sm hover:variant-filled-error" on:click={() => removeSpell($activeSpellbookIndex, si)}>Remove</button>
+                                                <p><b>School</b> {s.level} {s.school.toLowerCase()} {s.can_be_cast_as_ritual ? "(ritual)" : ""}</p>
+                                                <p><b>Casting Time</b> {s.casting_time}</p>
+                                                <p><b>Range</b> {s.range}</p>
+                                                <p><b>Components</b> {s.components}</p>
+                                                {#if s.requires_material_components}
+                                                    <p><b>Materials</b> {s.material}</p>
+                                                {/if}
+                                                <p>
+                                                    <b>Duration</b>
+                                                    {s.requires_concentration ? "Concentration," : ""}
+                                                    {s.duration.toLowerCase()}
+                                                </p>
+                                            </div>
+                                            <p class="pt-2 indent-4">{s.desc}</p>
+                                            {#if s.higher_level}
+                                                <p class="pt-2"><b>At Higher Levels</b> {s.higher_level}</p>
                                             {/if}
-                                            <p>
-                                                <b>Duration</b>
-                                                {s.requires_concentration ? "Concentration," : ""}
-                                                {s.duration.toLowerCase()}
-                                            </p>
+
                                         </div>
-                                        <p class="pt-2 indent-4">{s.desc}</p>
-                                        {#if s.higher_level}
-                                            <p class="pt-2"><b>At Higher Levels</b> {s.higher_level}</p>
-                                        {/if}
                                     </svelte:fragment>
                                 </AccordionItem>
                             </div>
                         {/each}
-                        <br>
-                        <div class="flex">
-                            <button class="w-full btn variant-soft-error hover:variant-filled-error" type="button" on:click={() => deleteSpellbook($activeSpellbookIndex)}>Delete {$spellbook[$activeSpellbookIndex].name}'s Spellbook</button>
+
+                        <div class="flex justify-end pt-8">
+                            <button class="btn variant-soft hover:variant-filled-error" type="button"
+                                    on:click={() => deleteSpellbook($activeSpellbookIndex)}>
+                                Delete <span class="px-1 text-primary-500">{$spellbook[$activeSpellbookIndex].name}'s</span> Spellbook
+                            </button>
                         </div>
                     {/if}
                 </Accordion>
