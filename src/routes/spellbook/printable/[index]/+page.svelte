41f2206210fe2d49a917cf2bc7d671285c415c7c
Bjørn Nørgaard <bjornsnorgaard@gmail.com>
2024-10-17 16:14:22 +0200

    Reformatted the printable page

--- src/routes/spellbook/printable/[index]/+page.svelte
+++ src/routes/spellbook/printable/[index]/+page.svelte
@@ -2,7 +2,6 @@
     import { spellbookStore } from "$lib/stores/spellbookStore";
     import PrintablePage from "../PrintablePage.svelte";
     import type { Spell } from "$lib/types/spell";
-    import { onMount } from "svelte";
 
     export let data;
     let spellbook;
@@ -19,10 +18,9 @@
             }
             groupedSpells[level].push(spell);
         }
-        spellsByLevel = [...groupedSpells]; 
+        spellsByLevel = [ ...groupedSpells ];
     }
 </script>
-
 
 <PrintablePage title={`${spellbook.name}'s Spellbook`}>
     <div class="space-y-4">
@@ -37,46 +35,45 @@
 
         {#if spellbook}
             {#each spellsByLevel as level}
-                {#if level && level.length>0}
-                    <p class="font-bold border-b-2 border-black w-full pb-1 text-2xl ">Level {level[0].level_int}</p>
-                        {#each level as s}
-                            <div class="break-inside-avoid-page">
-                                <div>
-                                    <p class="text-2xl font-bold">{s.name}</p>
-                                    {#if s.level_int === 0}
-                                        {s.school} cantrip
-                                    {:else}
-                                        {s.level} {s.school.toLowerCase()}
-                                    {/if}
-                                    {#if s.can_be_cast_as_ritual} ritual{/if}
-                                </div>
-
-                                <div>
-                                    <p><b>Casting Time:</b> {s.casting_time}</p>
-                                    <p><b>Range:</b> {s.range}</p>
-                                    <p><b>Components:</b> {s.components}</p>
-                                    {#if s.requires_material_components && s.material.length}
-                                        <p><b>Material:</b> {s.material}</p>
-                                    {/if}
-                                    {#if s.requires_concentration}
-                                        <p><b>Duration:</b> Concentration, {s.duration.toLowerCase()}</p>
-                                    {:else if s.duration.toLowerCase() === "instantaneous"}
-                                        <p><b>Duration:</b> Instant</p>
-                                    {:else}
-                                        <p><b>Duration</b> {s.duration}</p>
-                                    {/if}
-                                </div>
-
-                                <p>{s.desc}</p>
-
-                                {#if s.higher_level}
-                                    <p><b>At higher levels:</b> {s.higher_level}</p>
+                {#if level && level.length > 0}
+                    <p class="w-full border-b-2 border-black pb-1 text-2xl font-bold">Level {level[0].level_int}</p>
+                    {#each level as s}
+                        <div class="break-inside-avoid-page">
+                            <div>
+                                <p class="text-2xl font-bold">{s.name}</p>
+                                {#if s.level_int === 0}
+                                    {s.school} cantrip
+                                {:else}
+                                    {s.level} {s.school.toLowerCase()}
                                 {/if}
-
+                                {#if s.can_be_cast_as_ritual} ritual{/if}
                             </div>
-                        {/each}
-                    {/if}
-                {/each}
-            {/if}
+
+                            <div>
+                                <p><b>Casting Time:</b> {s.casting_time}</p>
+                                <p><b>Range:</b> {s.range}</p>
+                                <p><b>Components:</b> {s.components}</p>
+                                {#if s.requires_material_components && s.material.length}
+                                    <p><b>Material:</b> {s.material}</p>
+                                {/if}
+                                {#if s.requires_concentration}
+                                    <p><b>Duration:</b> Concentration, {s.duration.toLowerCase()}</p>
+                                {:else if s.duration.toLowerCase() === "instantaneous"}
+                                    <p><b>Duration:</b> Instant</p>
+                                {:else}
+                                    <p><b>Duration</b> {s.duration}</p>
+                                {/if}
+                            </div>
+
+                            <p>{s.desc}</p>
+
+                            {#if s.higher_level}
+                                <p><b>At higher levels:</b> {s.higher_level}</p>
+                            {/if}
+                        </div>
+                    {/each}
+                {/if}
+            {/each}
+        {/if}
     </div>
 </PrintablePage>
