7370de9dffe69a57c69154f8ea46b366023a193e
Bjørn Nørgaard <bjornsnorgaard@gmail.com>
2024-12-05 20:53:40 +0100

    Removed characters page

--- src/routes/characters/+page.svelte
+++
@@ -1,7 +0,0 @@
-<script>
-    import PageWrapper from "$lib/components/PageWrapper.svelte";
-</script>
-
-<PageWrapper title="Characters" desc="Create and manage your own characters">
-    <p>Here you'll be able to manage your own characters - eventually...</p>
-</PageWrapper>
